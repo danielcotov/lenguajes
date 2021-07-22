f8de0751509239e57b89cab4fd5f0606dfdbbfed
Daniel Coto <danielcotov@gmail.com>
2021-07-21 18:22:03 -0600

    index page with icons and buttons

--- index.php
+++ index.php
@@ -30,6 +30,8 @@
   <link href="css/simple-sidebar.css" rel="stylesheet">
   <link href="css/estilosSitio.css" rel="stylesheet">
   <link rel="stylesheet" href="resources/styles/styles.css">
+  
+  
 
 </head>
 
@@ -39,24 +41,15 @@
         <div class="bg-light border-right" id="sidebar-wrapper">
             <div class="sidebar-heading">Lenguajes de Base de Datos</div>
             <div class="list-group list-group-flush">
-                <a href="index.php" class="list-group-item list-group-item-action bg-light">Consulta</a>
-                <a href="compra.php" class="list-group-item list-group-item-action bg-light">Compra</a>
+                <a href="index.php" class="list-group-item list-group-item-action bg-light">Inicio</a>
+                <a href="compra.php" class="list-group-item list-group-item-action bg-light">Empleados</a>
+                <a href="compra.php" class="list-group-item list-group-item-action bg-light">Productos</a>
+                <a href="compra.php" class="list-group-item list-group-item-action bg-light">Clientes</a>
+                <a href="compra.php" class="list-group-item list-group-item-action bg-light">Reportes</a>
             </div>
         </div>
+    
     <div id="page-content-wrapper">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-            <button class="btn btn-primary" id="menu-toggle">Menu</button>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Salir <span class="sr-only">(current)</span></a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
         <div class="header" style="background-color: #0071CE">
             <div class="container">
                <div class="header_logo">
@@ -65,52 +58,136 @@
             </div>
         </div>
         <br>
-        <div class="container-fluid">
+        <div class="container">
             <div class="row">
-                <div class="col-4"></div>
-                <div class="col-4">
-                    <input type="submit" class="btn btn-success btn-block" id="btnConsultar" name="btnConsultar" value="Consultar Casas Vendidas"/>
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                    <div class="row">
+                        <div class="col-6">
+                            <div class="categories_item">
+                                <div class="categories_item_thumb">
+                                    <div class="categories_overlay">
+                                        <img src="resources/imgs/employee.png" alt="Employee" style="margin-left: 15%; height: 50%; width: 50%">
+                                        <div class="d-flex justify-content-center" style="margin-left: -15%; margin-top: 2.5%">
+                                            <a href="<%=request.getContextPath()%>/employees" class="btn btn-success" style="color: #fff; background-color: #FFC220; border-color: #FFC220;">Empleados</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                 
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="categories_item">
+                                <div class="categories_item_thumb">
+                                    <div class="categories_overlay">
+                                        <img src="resources/imgs/customer.png" alt="Customer" style="margin-left: 15%; height: 50%; width: 50%">
+                                        <div class="d-flex justify-content-center" style="margin-left: -15%; margin-top: 2.5%">
+                                            <a href="<%=request.getContextPath()%>/employees" class="btn btn-success" style="color: #fff; background-color: #FFC220; border-color: #FFC220;">Clientes</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+                    <br>
+                    <div class="row">
+                        <div class="col-6">
+                            <div class="categories_item">
+                                <div class="categories_item_thumb">
+                                    <div class="categories_overlay">
+                                        <img src="resources/imgs/employee.png" alt="Employee" style="margin-left: 15%; height: 50%; width: 50%">
+                                        <div class="d-flex justify-content-center" style="margin-left: -15%; margin-top: 2.5%">
+                                            <a href="<%=request.getContextPath()%>/employees" class="btn btn-success" style="color: #fff; background-color: #FFC220; border-color: #FFC220;">Empleados</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                 
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="categories_item">
+                                <div class="categories_item_thumb">
+                                    <div class="categories_overlay">
+                                        <img src="resources/imgs/customer.png" alt="Customer" style="margin-left: 15%; height: 50%; width: 50%">
+                                        <div class="d-flex justify-content-center" style="margin-left: -15%; margin-top: 2.5%">
+                                            <a href="<%=request.getContextPath()%>/employees" class="btn btn-success" style="color: #fff; background-color: #FFC220; border-color: #FFC220;">Clientes</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-4"></div>
             </div>
-            <br/>
-            <table class="table table-bordered">
-                <thead>
-                    <th>ID Venta</th>
-                    <th>Nombre Vendedor</th>
-                    <th>Descripción Casa</th>
-                    <th>Precio</th>
-                    <th>Fecha Venta</th>
-                </thead>
-                <tbody>
-                    <?php
-                        if(empty($listaVentas))
-                        {
-                            echo '<tr>';
-                            echo '<td colspan="7">Sin Resultados</td>';
-                            echo '</tr>';
-                        }
-                        else
-                        {
-                            While($row = mysqli_fetch_array($listaVentas))
-                            {
-                                echo '<tr>';
-                                echo '<td>'. $row["IdVenta"] .'</td>';
-                                echo '<td>'. $row["NombreVendedor"] .'</td>';
-                                echo '<td>'. $row["DescripcionCasa"] .'</td>';
-                                echo '<td>'. $row["PrecioCasa"] .'</td>';
-                                echo '<td>'. $row["FechaVenta"] .'</td>';
-                                
-                            }
-                        }
-                    ?>
-            </table>
-            
         </div>
+        <div id="content" class="p-4 p-md-5 pt-5">
+                <div class="containericon" style="top: 100px; left: 400px">
+                    <img src="employeeicon" alt="Employee" class="image" style="top: 100px; left: 400px; width: 100%;"/>
+                    <div class="middle" style="  top: 115%; left: 50%;">
+                        <div> 
+                            <a href="<%=request.getContextPath()%>/employees" class="btn btn-success">Employees</a>
+                        </div>                       
+                    </div>
+                </div>
+                <div class="containericon" style="top: 100px; left: 750px">
+                    <img src="customericon" alt="Customer" class="image" style="top: 100px; left: 750px; width: 100%;"/>
+                    <div class="middle" style="  top: 115%; left: 50%;">
+                        <div> 
+                            <a href="<%=request.getContextPath()%>/customers" class="btn btn-success">Customers</a>
+                        </div>                       
+                    </div>
+                </div>
+                <div class="containericon" style="top: 100px; left: 1100px">
+                    <img src="producticon" alt="Product" class="image" style="top: 100px; left: 1100px; width: 100%;"/>
+                    <div class="middle" style="  top: 115%; left: 50%;">
+                        <div> 
+                            <a href="<%=request.getContextPath()%>/products" class="btn btn-success">Products</a>
+                        </div>                       
+                    </div>
+                </div>
+                <div class="containericon" style="top: 100px; left: 1450px">
+                    <img src="saleicon" alt="Sale" class="image" style="top: 100px; left: 1450px; width: 100%;"/>
+                    <div class="middle" style="  top: 115%; left: 50%;">
+                        <div> 
+                            <a href="<%=request.getContextPath()%>/sale" class="btn btn-success">Sale</a>
+                        </div>                       
+                    </div>
+                </div>
+                <div class="containericon" style="top: 500px; left: 400px">
+                    <img src="reporticon" alt="Report" class="image" style="top: 500px; left: 400px; width: 100%;"/>
+                    <div class="middle" style="  top: 115%; left: 50%;">
+                        <div> 
+                            <a href="<%=request.getContextPath()%>/report-list" class="btn btn-success">Reports</a>
+                        </div>                       
+                    </div>
+                </div>
+                <div class="containericon" style="top: 500px; left: 750px">
+                    <img src="profileicon" alt="Profile" class="image" style="top: 500px; left: 750px; width: 100%;"/>
+                    <div class="middle" style="  top: 115%; left: 50%;">
+                        <div> 
+                            <a href="<%=request.getContextPath()%>/profile" class="btn btn-success">Profile</a>
+                        </div>                       
+                    </div>
+                </div>
+                <div class="containericon" style="top: 500px; left: 1100px">
+                    <img src="graphicon" alt="Charts" class="image" style="top: 500px; left: 1100px; width: 100%;"/>
+                    <div class="middle" style="  top: 115%; left: 50%;">
+                        <div> 
+                            <a href="<%=request.getContextPath()%>/charts" class="btn btn-success">Charts</a>
+                        </div>                       
+                    </div>
+                </div>
+                <div class="containericon" style="top: 500px; left: 1450px">
+                    <img src="logouticon" alt="Logout" class="image" style="top: 500px; left: 1450px; width: 100%;"/>
+                    <div class="middle" style="  top: 115%; left: 50%;">
+                        <div> 
+                            <a href="<%=request.getContextPath()%>/login" class="btn btn-success">Logout</a>
+                        </div>                       
+                    </div>
+                </div>
+            </div>  
     </div>
-
-  </div>
-
+    </div>
   <script src="vendor/jquery/jquery.min.js"></script>
   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="js/funcionesSitio.js"></script>
@@ -120,9 +197,7 @@
       e.preventDefault();
       $("#wrapper").toggleClass("toggled");
     });
-      
 
-        
   </script>
 </form>
 </body>
