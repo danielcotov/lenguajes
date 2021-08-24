155d2a1250d28a32598c28de199c7510de5f925c
deathstars22 <josgabfer@outlook.com>
2021-08-23 22:09:38 -0600

    NEW DB

--- resources/conexionBD.php
+++ resources/conexionBD.php
@@ -1,10 +1,9 @@
 <?php
  
     $db = "(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1521)))(CONNECT_DATA=(SID=orcl)))" ;
-    $conn = OCILogon("gadafa", "gadafa", $db) or die;
+    $conn = OCILogon("gadafa", "GADAFA", $db) or die;
     $sql = "ALTER SESSION SET NLS_DATE_FORMAT = 'DD-MM-YYYY'";
     $parse = oci_parse($conn, $sql);
     oci_execute($parse);
     oci_free_statement($parse);
-
 ?>
